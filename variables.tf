2d37084b0a07afc1d2f17109df18166fe9a71f8c
ddimri <deepak_dimri@hotmail.com>
2016-11-07 19:02:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,63 +1,31 @@
-variable "home_dir" {
- default = "/home/ec2-user"
- }
+variable "home_dir" {}
 
-variable "aws_profile" {
- default = "freeipa"
- }
+variable "aws_profile" {}
 
-variable "project" {
-  default = "ipa"
-  }
+variable "project" {}
 
-variable "environment" {
-  default = "opswest"
-  }
+variable "environment" {}
 
-variable "vpc_cidr_block" {
-  default = "172.16.248.0/23"
-  }
+variable "vpc_cidr_block" {}
 
-variable "private_subnet1_cidr_block" {
-  default = "172.16.248.0/28"
-  }
+variable "private_subnet1_cidr_block" {}
 
-variable "private_subnet2_cidr_block" {
-  default = "172.16.248.16/28"
-  }
+variable "private_subnet2_cidr_block" {}
 
-variable "public_subnet1_cidr_block" {
-  default = "172.16.249.0/28"
-  }
+variable "public_subnet1_cidr_block" {}
 
-variable "public_subnet2_cidr_block" {
-  default = "172.16.249.16/28"
-  }
+variable "public_subnet2_cidr_block" {}
 
-variable "availability_zones" {
-  default = ["us-west-2a", "us-west-2b"]
-  }
+variable "availability_zones" {}
 
-variable "cc_jumpbox_ami" {
-  default = "ami-746aba14"
-  }
+variable "cc_jumpbox_ami" {}
 
-variable "ipa_server_ami" {
-  default = "ami-d2c924b2"
-  }
+variable "ipa_server_ami" {}
 
-variable "instance_type" {
-  default = "t2.micro"
-  }
+variable "instance_type" {}
 
-variable "key_name" {
-  default = "freeipa-key"
-  }
+variable "key_name" {}
 
-variable "aws_region" {
-  default = "us-west-2"
-  }
+variable "aws_region" {}
 
-variable "vpc_name" {
-  default = "ipa-vpc"
-  }
+variable "vpc_name" {}
