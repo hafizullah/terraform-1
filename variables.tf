a64855c151eca1f1742003533f9940574162a8f2
ddimri <deepak_dimri@hotmail.com>
2016-11-07 17:02:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,55 @@
-variable "project" {}
-variable "environment" {}
-variable "vpc_cidr_block" {}
-variable "private_subnet1_cidr_block" {}
-variable "private_subnet2_cidr_block" {}
-variable "public_subnet1_cidr_block" {}
-variable "public_subnet2_cidr_block" {}
-variable "availability_zones" {}
-variable "cc_jumpbox_ami" {}
-variable "ipa_server_ami" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "aws_region" {}
-variable "vpc_name" {}
+variable "project" {
+  default = "ipa"
+  }
+
+variable "environment" {
+  default = "opswest"
+  }
+
+variable "vpc_cidr_block" {
+  default = "172.16.248.0/23"
+  }
+
+variable "private_subnet1_cidr_block" {
+  default = "172.16.248.0/28"
+  }
+
+variable "private_subnet2_cidr_block" {
+  default = "172.16.248.16/28"
+  }
+
+variable "public_subnet1_cidr_block" {
+  default = "172.16.249.0/28"
+  }
+
+variable "public_subnet2_cidr_block" {
+  default = "172.16.249.16/28"
+  }
+
+variable "availability_zones" {
+  default = ["us-west-2a", "us-west-2b"]
+  }
+
+variable "cc_jumpbox_ami" {
+  default = "ami-746aba14"
+  }
+
+variable "ipa_server_ami" {
+  default = "ami-d2c924b2"
+  }
+
+variable "instance_type" {
+  default = "t2.micro"
+  }
+
+variable "key_name" {
+  default = "freeipa-key"
+  }
+
+variable "aws_region" {
+  default = "us-west-2"
+  }
+
+variable "vpc_name" {
+  default = "ipa-vpc"
+  }
