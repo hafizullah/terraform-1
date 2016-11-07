1da23ccd3620b267825e22ec29e21485f1a15fb5
ddimri <deepak_dimri@hotmail.com>
2016-11-07 18:30:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # AWS access details
 
 provider "aws" {
-  shared_credentials_file = "${var.aws_creds_path/.aws/credentials}"
+  shared_credentials_file = "${var.aws_creds_path}/.aws/credentials"
   profile = "${var.aws_profile}"
   region = "${var.aws_region}"
 }
