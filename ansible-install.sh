00dd916abbb0ac240c827c41bd6aacf589471f7c
ddimri <deepak_dimri@hotmail.com>
2016-11-08 18:58:45 +0530

    Update ansible-install.sh

--- ansible-install.sh
+++ ansible-install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -v
-sudo apt-get update && sudo apt-get install ansible -y                                                         
+sudo apt-get update && sudo apt-get install ansible -y
+mkdir sudo /etc/ansible/inventory
