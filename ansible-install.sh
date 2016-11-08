10ed410bc8b6fc4e943e0fb1fd0badab384a1fd6
ddimri <deepak_dimri@hotmail.com>
2016-11-08 20:02:28 +0530

    Update ansible-install.sh

--- ansible-install.sh
+++ ansible-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -v
 sudo apt-get update && sudo apt-get install ansible -y
-mkdir sudo /etc/ansible/inventory
-wget https://github.com/ddimri/ipa/blob/master/freeipa /etc/ansible/inventory
+sudo mkdir /etc/ansible/inventory
+sudo wget https://github.com/ddimri/ipa/blob/master/freeipa /etc/ansible/inventory
 #git clone https://github.com/ddimri/ipa/:freeipa /etc/ansible/inventory
-wget https://github.com/ddimri/ipa/blob/master/ec2-registration-ipa-master.yml ~/security/ipa
+sudo wget https://github.com/ddimri/ipa/blob/master/ec2-registration-ipa-master.yml ~/security/ipa
