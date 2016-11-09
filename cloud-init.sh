f4301ce021b8f891f6ab393070e7db336ae61ab9
ddimri <deepak_dimri@hotmail.com>
2016-11-09 13:34:25 +0530

    Update cloud-init.sh

--- cloud-init.sh
+++ cloud-init.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
 sudo apt-get update && sudo apt-get install ansible -y
-sudo mkdir /etc/ansible/inventory
-sudo wget https://github.com/ddimri/ipa/blob/master/freeipa /etc/ansible/inventory
-#git clone https://github.com/ddimri/ipa/:freeipa /etc/ansible/inventory
-sudo wget https://github.com/ddimri/ipa/blob/master/ec2-registration-ipa-master.yml ~/security/ipa
