852b3d911ea0775840a79044741fbaca8ab822b0
ddimri <deepak_dimri@hotmail.com>
2016-11-09 14:50:08 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
 output "ipa_vpc_id" {
-  value = "${module.mgmt-us-west-2.id}"
+  value = "${module.vpc.mgmt-us-west-2.id}"
 }
 
 output "ipa_security_group_id" {
-  value = "${module_group.opswest-ipa-sg.id}"
+  value = "${module.vpc.opswest-ipa-sg.id}"
 }
 
 output "ipa_2a_subnet_id" {
-  value = ["${module.us-west-2a-private-subnet.id}"]
+  value = ["${module.vpc.us-west-2a-private-subnet.id}"]
 }
 
 output "ipa_2b_subnet_id" {
-  value = ["${module.us-west-2b-private-subnet.id}"]
+  value = ["${module.vpc.us-west-2b-private-subnet.id}"]
 }
 
 output "ipa_master_private_dns" {
-  value = ["${module.ipa-master.private_dns}"]
+  value = ["${module.vpc.ipa-master.private_dns}"]
 }
 
 output "ipa_master_private_ip" {
-  value = ["${module.ipa-master.private_ip}"]
+  value = ["${module.vpc.ipa-master.private_ip}"]
 }
 
 output "ipa_replica_private_dns" {
-  value = ["${module.ipa-replica.private_dns}"]
+  value = ["${module.vpc.ipa-replica.private_dns}"]
 }
 
 output "ipa_replica_private_ip" {
-  value = ["${module.ipa-replica.private_ip}"]
+  value = ["${module.vpc.ipa-replica.private_ip}"]
 }
