a119734a138981f712896be7bda2400003421741
ddimri <deepak_dimri@hotmail.com>
2016-12-27 12:18:13 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
 output "ipa_vpc_id" {
-  value = "${aws_vpc.mgmt-us-west-2.id}"
+  value = "${aws_vpc.ipa-mgmt-vpc.id}"
 }
 
 output "ipa_security_group_id" {
-  value = "${aws_security_group.opswest-ipa-sg.id}"
+  value = "${aws_security_group.ipa-server-sg.id}"
 }
 
 output "ipa_2a_subnet_id" {
-  value = ["${aws_subnet.us-west-2a-private-subnet.id}"]
+  value = ["${aws_subnet.private-subnet1.id}"]
 }
 
 output "ipa_2b_subnet_id" {
-  value = ["${aws_subnet.us-west-2b-private-subnet.id}"]
+  value = ["${aws_subnet.private-subnet2.id}"]
 }
 
 output "ipa_master_private_dns" {
-  value = ["${aws_instance.ipa-master.private_dns}"]
+  value = ["${aws_instance.ipa-master-1.private_dns}"]
 }
 
 output "ipa_master_private_ip" {
-  value = ["${aws_instance.ipa-master.private_ip}"]
+  value = ["${aws_instance.ipa-master-1.private_ip}"]
 }
 
 output "ipa_replica_private_dns" {
-  value = ["${aws_instance.ipa-replica.private_dns}"]
+  value = ["${aws_instance.ipa-master-2.private_dns}"]
 }
 
 output "ipa_replica_private_ip" {
-  value = ["${aws_instance.ipa-replica.private_ip}"]
+  value = ["${aws_instance.ipa-master-2.private_ip}"]
 }
