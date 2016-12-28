93cdcb951ca46c3b01ab7f5919389f98cd02ef9c
ddimri <deepak_dimri@hotmail.com>
2016-12-28 19:31:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,6 +37,8 @@ variable "vpc_name" {}
 
 variable "feyedc_cidr_block" {}
 
+variable "remote_vpc_cidr_block" {}
+
 variable "private_key" {
   default = "~/.ssh/freeipa-key.pem"
   description = "private key to connect to hosts from the jumpbox"
