0e1ee0bde238bdcd9568ed8103a52a1a9f9f326c
ddimri <deepak_dimri@hotmail.com>
2017-01-02 15:41:26 +0530

    Update install-ansible.sh

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -v
 sudo -u ubuntu mkdir /home/ubuntu/security
 sudo -u ubuntu mkdir /home/ubuntu/security/ipa
+sudo -u ubuntu mkdir /home/ubuntu/security/inventory
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update && sudo apt-get install ansible -y
 sudo apt-get install yum
-sudo -u ubuntu mkdir /home/ubuntu/security/inventory
 sudo apt-get install python-pip -y
 sudo apt-get install python-boto -y
 sudo apt-get install git -y
